e6c852496a7deecfec19b0c3e1cefa3fa291aa4e
mousechannel <mochenghh@gmail.com>
2022-09-11 09:57:52 +0800

    kugi

--- publish.bat
+++ publish.bat
@@ -10,7 +10,7 @@ D:\my\Git\bin\git.exe add .
 D:\my\Git\bin\git.exe commit -m "%mes%"
 
 echo push MoChengFixedMath
-D:\my\Git\bin\git.exe subtree --prefix=Packages/FixedMath push git@github.com:MoChengl23/FixexMathPackage.git master
+D:\my\Git\bin\git.exe subtree --prefix=Packages/FixedMath push git@github.com:MoChengl23/MoChengFixdMath.git master
 echo push MoChengFixedMath
 D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengHttp push git@github.com:MoChengl23/MoChengHTTP.git main
 
