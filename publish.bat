f1f389491026602c2edd6c9ff7cb285259a91d7a
mousechannel <mochenghh@gmail.com>
2022-09-14 16:17:05 +0800

    FixedMath BUG

--- publish.bat
+++ publish.bat
@@ -9,13 +9,13 @@ D:\my\Git\bin\git.exe add .
  
 D:\my\Git\bin\git.exe commit -m "%mes%"
 
-echo -------push MoChengFixedMath-------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengFixedMath push git@github.com:MoChengl23/MoChengFixdMath.git master
-echo -------push MoChengFixedMath-------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengHttp push git@github.com:MoChengl23/MoChengHTTP.git master
+echo ------------push MoChengFixedMath------------
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengFixedMath push git@github.com:MouseChannle/MoChengFixdMath.git master
+echo ------------push MoChengFixedMath------------
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengHttp push git@github.com:MouseChannle/MoChengHTTP.git master
 
-echo -------push MoChengZip-------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengZip push git@github.com:MoChengl23/MoChengCompress.git master
+echo ------------push MoChengZip------------
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengZip push git@github.com:MouseChannle/MoChengCompress.git master
 
 D:\my\Git\bin\git.exe push -u origin master
 
