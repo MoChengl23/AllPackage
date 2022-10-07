e4ea6e0b9a2009ecb3f2298577b13dabe427ba3f
mousechannel <mochenghh@gmail.com>
2022-10-07 18:37:16 +0800

    add rvo pack

--- publish.bat
+++ publish.bat
@@ -10,16 +10,17 @@ D:\my\Git\bin\git.exe add .
 D:\my\Git\bin\git.exe commit -m "%mes%"
 
 echo ------------push MoChengFixedMath------------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengFixedMath push git@github.com:MouseChannle/MoChengFixdMath.git master
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengFixedMath push git@github.com:MouseChannel/MoChengFixdMath.git master
 echo ------------push MoChengHTTP------------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengHttp push git@github.com:MouseChannle/MoChengHTTP.git master
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengHttp push git@github.com:MouseChannel/MoChengHTTP.git master
 
 echo ------------push MoChengZip------------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengZip push git@github.com:MouseChannle/MoChengCompress.git master
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengZip push git@github.com:MouseChannel/MoChengCompress.git master
 
 echo ------------push MoChengClientNet------------
-D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengClientNet push git@github.com:MouseChannle/MoChengClientNet.git master
-
+D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengClientNet push git@github.com:MouseChannel/MoChengClientNet.git master
+echo ------------push MoChengRVO------------
+ D:\my\Git\bin\git.exe subtree --prefix=Packages/MoChengRVO add git@github.com:MouseChannel/MoChengRVO.git master
 
 
 D:\my\Git\bin\git.exe push -u origin master
